a799db206d1249016c497e4d56f860151a81a568
Govind <gvgupta789@gmail.com>
2024-11-28 16:39:09 +0530

    Update imageControllers.js

--- controllers/imageControllers.js
+++ controllers/imageControllers.js
@@ -4,44 +4,25 @@ const generateImage = async (req, res) => {
     const body = req.body;
     const searchText = body.searchText;
 
-    function getRandomLink(data) {
-        const randomItem = data.results[Math.floor(Math.random() * data.results.length)];
-        const urls = randomItem.urls;
-        const urlKeys = Object.keys(urls);
-        const randomKey = urlKeys[Math.floor(Math.random() * urlKeys.length)];
-        return urls[randomKey];
-    }
+    // function getRandomLink(data) {
+    //     const randomItem = data.results[Math.floor(Math.random() * data.results.length)];
+    //     const urls = randomItem.urls;
+    //     const urlKeys = Object.keys(urls);
+    //     const randomKey = urlKeys[Math.floor(Math.random() * urlKeys.length)];
+    //     return urls[randomKey];
+    // }
 
     let imageUrl = "";
     try{
-        const res = await fetch(`https://unsplash.com/napi/search/illustrations/related?query=${searchText}`, {
-            "headers": {
-              "accept": "*/*",
-              "accept-language": "en-US",
-              "client-geo-region": "global",
-              "if-none-match": "W/\"4f53cda18c2baa0c0354bb5f9a3ecbe5\"",
-              "priority": "u=1, i",
-              "sec-ch-ua": "\"Brave\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
-              "sec-ch-ua-mobile": "?0",
-              "sec-ch-ua-platform": "\"Windows\"",
-              "sec-fetch-dest": "empty",
-              "sec-fetch-mode": "cors",
-              "sec-fetch-site": "same-origin",
-              "sec-gpc": "1"
-            },
-            "referrer": `https://unsplash.com/s/photos/${searchText}`,
-            "referrerPolicy": "origin-when-cross-origin",
-            "body": null,
-            "method": "GET",
-            "mode": "cors",
-            "credentials": "include"
-          });
-        const data  = await res.json();
-        imageUrl = getRandomLink(data);
+        const res = await fetch(`https://api.unsplash.com/photos/random?query=${searchText}&client_id=${process.env.UNSPLASH_ACCESS_KEY}`);
+
+        const data  = res.url;
+        // imageUrl = getRandomLink(data);
         
 
         await imageModel.create({
             searchText: searchText,
+            url : data
         });
     }
     
